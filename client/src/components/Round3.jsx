d3cc0110195966ef59b24552837021216ba08732
13Anisha <119130741+13Anisha@users.noreply.github.com>
2024-09-25 03:54:22 +0530

    Update Round3.jsx

--- client/src/components/Round3.jsx
+++ client/src/components/Round3.jsx
@@ -21,7 +21,7 @@ const Round3 = () => {
     console.log('Submitting Team ID:', teamId);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/round3/submit', formData, {
+      const response = await axios.post('https://switchuation.onrender.com/api/round3/submit', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
